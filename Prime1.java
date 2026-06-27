0b9280553f69fa08b109bea81da1d13bb1f9b27d
Dr.B.Nanditha Boddu <nanditha.boddu15@gmail.com>
2026-06-27 15:32:52 +0530

    Refactor Prime1 class and improve output formatting
    Refactored output to use 'true' and 'false' strings. Removed unnecessary comments and cleaned up code structure.

--- Prime1.java
+++ Prime1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Prime1 {
 
     static boolean isPrime(int n) {
@@ -13,38 +12,14 @@ class Prime1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(11));
-        System.out.println(isPrime(15));
-    }
-}
-=======
-class Prime1
-{
-    static boolean isPrime(int n)
-    {
-        // Corner case
-        if (n <= 1)
-            return false;
-
-        // Check from 2 to n-1
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
-                return false;
-
-        return true;
-    }
-
-    // Driver Program
-    public static void main(String args[])
-    {
         if (isPrime(11))
-            System.out.println(" true");
+            System.out.println("true");
         else
-            System.out.println(" false");
+            System.out.println("false");
+
         if (isPrime(15))
-            System.out.println(" true");
+            System.out.println("true");
         else
-            System.out.println(" false");
+            System.out.println("false");
     }
->>>>>>> feature
 }
